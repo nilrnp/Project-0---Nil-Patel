cb0c17b832050b1154e6f5ec6c99e0592dd5a5c6
Nil Patel <nilrnp@gmail.com>
2022-01-25 13:52:36 -0800

    removed static calculator

--- Run_staticCalculator.java
+++ Run_staticCalculator.java
@@ -12,12 +12,14 @@ public class Run_staticCalculator {
 		System.out.println("Division: " + Calculator.Division());
 		System.out.println("Remainder: " + Calculator.Remainder() + "\n");
 
-		System.out.println("Overloaded Static Method");
-		System.out.println("Addition: " + Calculator.Addition(10, 15));
-		System.out.println("Subtraction: " + Calculator.Subtraction(20, 13));
-		System.out.println("Multiplication: " + Calculator.Multiplication(4, 8));
-		System.out.println("Division: " + Calculator.Division(20, 5));
-		System.out.println("Remainder: " + Calculator.Remainder(7, 30) + "\n");
+//		System.out.println("Overloaded Static Method");
+//		System.out.println("Addition: " + Calculator.Addition(10, 15));
+//		System.out.println("Subtraction: " + Calculator.Subtraction(20, 13));
+//		System.out.println("Multiplication: " + Calculator.Multiplication(4, 8));
+//		System.out.println("Division: " + Calculator.Division(20, 5));
+//		System.out.println("Remainder: " + Calculator.Remainder(7, 30) + "\n");
+		
+		
 	}
 
 }
